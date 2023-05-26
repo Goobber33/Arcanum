bbce30d3a42106aaacf87fc3f00a3fa9b17f7654
Kyle <kcparks1234@gmail.com>
2023-05-26 11:48:00 -0700

    Finished adding the transition from the login to the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,33 +2,45 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const HomePage = () => {
-  const transition = {
-    duration: 0.5,
-    ease: [0.43, 0.13, 0.23, 0.96]
-  };
+    const pageVariants = {
+      initial: {
+        y: "100%",
+        opacity: 0
+      },
+      animate: {
+        y: "0%",
+        opacity: 1
+      },
+      exit: {
+        y: "-100%",
+        opacity: 0
+      }
+    };
+  
+    const pageTransition = {
+      type: "tween",
+      ease: "anticipate",
+      duration: 0.5
+    };
+  
+    const style = {
+      backgroundColor: '#f0f0f0',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
 
-  const variants = {
-    exit: { y: "100%", opacity: 0, transition },
-    enter: { y: "0%", opacity: 1, transition }
-  };
-
-  const style = {
-    backgroundColor: '#f0f0f0',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  };
-
-  return (
-    <motion.div
-      initial="exit"
-      animate="enter"
-      exit="exit"
-      variants={variants}
-      style={style}
-    >
+    return (
+        <motion.div
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={pageVariants}
+          transition={pageTransition}
+          style={style}
+        >
       <h1>Welcome to the Homepage!</h1>
       <p>This is a basic homepage component.</p>
     </motion.div>
