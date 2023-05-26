196e10df8b19a1dad33bb37e975fc97dee6c49aa
Kyle <kcparks1234@gmail.com>
2023-05-26 09:38:00 -0700

    Added framer motion package to smoothly transition the page from the login page to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,52 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const pageVariants = {
+    exit: {
+      y: "100vh",
+      opacity: 0
+    },
+    enter: {
+      y: "0",
+      opacity: 1
+    }
+  };
+  
+  const pageTransition = {
+    type: "tween",
+    ease: "anticipate",
+    duration: 0.5
+  };
+
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          {/* Add more routes as needed */}
-        </Routes>
+        <AnimatePresence mode='wait'>
+          <Routes>
+            <Route 
+              path="/" 
+              element={isLoggedIn 
+                ? <Navigate to="/home" /> 
+                : <LoginPage variants={pageVariants} transition={pageTransition} onLogin={handleLogin} />} 
+            />
+            <Route 
+              path="/home" 
+              element={isLoggedIn 
+                ? <HomePage variants={pageVariants} transition={pageTransition} /> 
+                : <Navigate to="/" />} 
+            />
+          </Routes>
+        </AnimatePresence>
       </div>
     </Router>
   );
