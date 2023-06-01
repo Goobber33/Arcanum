86ead678307dcd6605b42a579b3bf349404055e0
Kyle <kcparks1234@gmail.com>
2023-06-01 15:38:10 -0700

    Added a create game, join game, and cancel button to the start button.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import mainImage from './test.png';
 
 const HomePage = ({ onLogout }) => {
     const navigate = useNavigate();
+    const [startClicked, setStartClicked] = useState(false);
+
     const pageVariants = {
         initial: {
             y: "100%",
@@ -77,6 +79,24 @@ const HomePage = ({ onLogout }) => {
         transform: 'translateX(-50%)'
     };
 
+    const startGame = () => {
+        setStartClicked(true);
+    }
+
+    const cancelGame = () => {
+        setStartClicked(false);
+    }
+
+    const buttonVariants = {
+        hidden: { opacity: 0, y: "-100%" },
+        visible: { opacity: 1, y: 0 },
+    };
+
+    const gameButtonStyle = {
+        width: '150px',
+        marginBottom: '10px'
+    };
+
     return (
         <motion.div
             initial="initial"
@@ -90,10 +110,48 @@ const HomePage = ({ onLogout }) => {
             <button className="btn btn-primary" onClick={onLogout} style={logoutButtonStyle}>Logout</button>
             <button className="btn btn-primary" onClick={() => navigate('/profile')} style={profileButtonStyle}>Profile</button>
             <div style={containerStyle}>
-                <div className="start-button">
-                    <span className="tooltip">Good Luck!</span>
-                    <span>START</span>
-                </div>
+                {!startClicked ? (
+                    <div className="start-button" onClick={startGame}>
+                        <span className="tooltip">Good Luck!</span>
+                        <span>START</span>
+                    </div>
+                ) : (
+                    <AnimatePresence>
+                        <motion.button
+                            className="btn btn-primary"
+                            onClick={() => navigate('/create')}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={buttonVariants}
+                            style={gameButtonStyle}
+                        >
+                            Create Game
+                        </motion.button>
+                        <motion.button
+                            className="btn btn-primary"
+                            onClick={() => navigate('/join')}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={buttonVariants}
+                            style={gameButtonStyle}
+                        >
+                            Join Game
+                        </motion.button>
+                        <motion.button
+                            className="btn btn-secondary"
+                            onClick={cancelGame}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={buttonVariants}
+                            style={gameButtonStyle}
+                        >
+                            Cancel
+                        </motion.button>
+                    </AnimatePresence>
+                )}
             </div>
         </motion.div>
     );
