7a603f230819165170fb06b485232c8fea37cc88
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-06-05 18:28:23 -0700

    changed gameboard

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,81 +1,59 @@
-import React from 'react'
-// import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import './css/gamepage.css'; // Import the CSS file for styling
 
+const Game = () => {
+  const [player1Spaces, setPlayer1Spaces] = useState(Array(4).fill(null));
+  const [player2Spaces, setPlayer2Spaces] = useState(Array(4).fill(null));
+  const [player1Deck, setPlayer1Deck] = useState(['card1', 'card2', 'card3']); // Replace with actual card data
+  const [player2Deck, setPlayer2Deck] = useState(['card4', 'card5', 'card6']); // Replace with actual card data
 
-  
-    
-    function generateGrid(rows,columns,mapper){
-    return Array(rows)
-    .fill()
-    .map(() => Array(columns).fill().map(mapper))
-    
-    }
-    
-    const newWarBattleGrid = () =>
-     generateGrid(7,5,()=>null)
-    
-    
-  export default function GamePage() {
-         const grid = newWarBattleGrid()
-         const handleClick = (rowIdx, colIdx) => {
-          console.log(`Clicked on cell (${rowIdx}, ${colIdx})`);
-          // Perform any desired logic or state updates here
-        };
-         
-         console.log(grid)
-         return(
-         <div style={{}}>
-            <Grid grid={grid} handleClick={handleClick}/>
-         </div>
-     )}
-    
-    
-
-function Grid({ grid }) {
-    return (
-      // Wrapping the grid with a div of inline-block means that the grid
-      // takes up only the space defined by the size of the cells, while
-      // still allowing us to use fractional values for the grid-template-*
-      // properties
-      <div style={{ display: 'inline-block' }}>
-        <div
-          style={{
-            // We set a background color to be revealed as the lines
-            // of the board with the `grid-gap` property
-            backgroundColor: '#000',
-            display: 'grid',
-            // Our rows are equal to the length of our grid
-            gridTemplateRows: `repeat(${grid.length}, 1fr)`,
-            // Our columns are equal to the length of a row
-            gridTemplateColumns: `repeat(${grid[0].length}, 1fr)`,
-            gridGap: 2,
-          }}
-        >
-          {grid.map((row, rowIdx) =>
-            row.map((cell, colIdx) => (
-              // We put the colIdx first because that is our X-axis value
-              // and the rowIdx second because that is our Y-axis value
-              // Getting in the habit makes using 2d grids much easier
-              <Cell key={`${colIdx}-${rowIdx}`}  key={`${colIdx}-${rowIdx}`}
-              cell={cell}
-              onClick={() => handleClick(rowIdx, colIdx)}
-            />
-            ))
-          )}
+  return (
+    <div className="game">
+      <div className="player-area">
+        <h2>Player 1</h2>
+        <div className="spaces">
+          {player1Spaces.map((card, index) => (
+            <div key={index} className="space">
+              {card}
+            </div>
+          ))}
         </div>
       </div>
-    )
-  }
-  
-  const cellStyle = {
-    backgroundColor: '#fff',
-    height: 100,
-    width: 150,
-  }
-  
-  function Cell({ cell }) {
-    return <div style={cellStyle}>{cell}</div>
-  }
 
-// ==================================================================================================================================================  
-    
+      <div className="player-area">
+        <h2>Player 2</h2>
+        <div className="spaces">
+          {player2Spaces.map((card, index) => (
+            <div key={index} className="space">
+              {card}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="deck">
+        <h3>Player 1 Deck</h3>
+        <div className="card-deck">
+          {player1Deck.map((card, index) => (
+            <div key={index} className="card">
+              {card}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="deck">
+        <h3>Player 2 Deck</h3>
+        <div className="card-deck">
+          {player2Deck.map((card, index) => (
+            <div key={index} className="card">
+              {card}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
