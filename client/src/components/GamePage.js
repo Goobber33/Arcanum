e21616cde7bb7eb0e23f9fadcc77cbc3c8b88564
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-06-05 18:44:15 -0700

    fixed the player sides

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -6,9 +6,22 @@ const Game = () => {
   const [player2Spaces, setPlayer2Spaces] = useState(Array(4).fill(null));
   const [player1Deck, setPlayer1Deck] = useState(['card1', 'card2', 'card3']); // Replace with actual card data
   const [player2Deck, setPlayer2Deck] = useState(['card4', 'card5', 'card6']); // Replace with actual card data
+  const [player1Hand, setPlayer1Hand] = useState(['card7', 'card8', 'card9']); // Replace with actual card data
+  const [player2Hand, setPlayer2Hand] = useState(['card10', 'card11', 'card12']); // Replace with actual card data
 
   return (
     <div className="game">
+      <div className="deck">
+        <h3>Player 1 Hand</h3>
+        <div className="card-deck">
+          {player1Deck.map((card, index) => (
+            <div key={index} className="card">
+              {card}
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className="player-area">
         <h2>Player 1</h2>
         <div className="spaces">
@@ -31,19 +44,9 @@ const Game = () => {
         </div>
       </div>
 
-      <div className="deck">
-        <h3>Player 1 Deck</h3>
-        <div className="card-deck">
-          {player1Deck.map((card, index) => (
-            <div key={index} className="card">
-              {card}
-            </div>
-          ))}
-        </div>
-      </div>
 
       <div className="deck">
-        <h3>Player 2 Deck</h3>
+        <h3>Player 2 Hand</h3>
         <div className="card-deck">
           {player2Deck.map((card, index) => (
             <div key={index} className="card">
